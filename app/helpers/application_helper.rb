1ed8d8a3c050c10018ee12657429fcd75b508b36
Thu Quach <thuquach90@gmail.com>
2013-07-29 10:23:39 -0700

    Add title functionality for show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "My Small Goal").join(" - ")
+      end
+    end
+  end
 end
--- spec/integration/creating_goals_spec.rb
+++ spec/integration/creating_goals_spec.rb
@@ -4,8 +4,12 @@ feature 'Creating Goals' do
   scenario "can create a goal" do
     visit '/'
     click_link 'New Goal'
-    fill_in 'Name', :with => 'Sleep early'
+    fill_in 'Name', :with => 'Sleep Early'
     click_button 'Create Goal'
     page.should have_content('Goal has been created.')
+    goal = Goal.find_by_name("Sleep Early")
+    page.current_url.should == goal_url(goal)
+    title = "Sleep Early - My Small Goal"
+    find("title").should have_content(title)
   end
 end
