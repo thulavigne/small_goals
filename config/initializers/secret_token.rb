a2165266e94d254b3ea2f12ca4c7c45727ef0ccc
Thu Quach <thuquach90@gmail.com>
2013-07-29 09:41:51 -0700

    Generate the Small Goals app using Rails 3.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmallGoals::Application.config.secret_token = '2ef64fd2b4312541aa599e95b3261b50c63f2f8e4f1b618e8d5cdb8fbe5c5ba6848907de5c0f7c55f6feab59b0da07898dcbefd1c80c56579e7bcedda5a6d91b'
